419b646f4aeefd5436af36651523bfc6938f5a11
MichaelCQ <michaelc9802@gmail.com>
2020-01-17 16:32:11 -0800

    Update board.js

--- Components/Board/board.js
+++ Components/Board/board.js
@@ -1,2 +1,16 @@
 import React from "react";
 import Block from "../Block/block";
+
+export default class Board extends React.Component {
+    Block(i) {
+      return (
+        <Block
+          value={this.props.blocks[i]}
+          onClick={() => this.props.onClick(i)}
+        />
+      );
+    }
+
+
+    
+}
