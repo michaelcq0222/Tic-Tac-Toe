40edf0102ebaec1fca2cbc5ec9765a066f73648b
MichaelCQ <michaelc9802@gmail.com>
2020-01-17 18:24:40 -0800

    game

--- Components/Board/board-test.js
+++ Components/Board/board-test.js
@@ -5,3 +5,11 @@ it('renders without crashing', () => {
     let blocks = Array(9).fill(null)
     shallow(<Board blocks={blocks}/>);
   });
+
+it('calls onClick event on click of a board square', () =>{
+    let blocks = Array(9).fill(null)
+    const onClick = jest.fn();
+    let wrapper = mount(<Board blocks={blocks} onClick={onClick}/>);
+    wrapper.find('button.block').first().simulate('click');
+    expect(onClick).toBeCalledWith(0)
+  })
