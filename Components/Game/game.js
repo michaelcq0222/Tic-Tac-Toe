2483648f5de3045fff97f313e425fcc57733313e
MichaelCQ <michaelc9802@gmail.com>
2020-01-17 19:30:02 -0800

    Game board and css for button

--- Components/Game/game.js
+++ Components/Game/game.js
@@ -10,7 +10,37 @@ export default class Game extends React.Component {
             blocks: Array(9).fill(null)
           }
         ],
+        step: 0,
+        X: true
       };
     }
+
+    render() {
+        let status;
+        if (winner) {
+        status = "Winner: " + winner;
+        } else {
+        if (this.state.step === 9) {
+            status = "It's a Tie!";
+        } else {
+            status = "Next Move: " + (this.state.X ? "X" : "O");
+        }
+        }
+        return (
+            <div className="game">
+              <div className="game-board">
+                <Board
+                  blocks={this.state.history[this.state.step].blocks}
+                  onClick={i => this.handleClick(i)}
+                />
+              </div>
+              <div className="game-status">
+                <div>{status}</div>
+                <button onClick={() => this.reset()}>New Game</button>
+                <button onClick={() => this.undo()}>Undo</button>
+              </div>
+            </div>
+          );
+        }
 
 }
