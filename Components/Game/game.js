52b8ba7862966981958837d3db15eb70c03d2cb4
MichaelCQ <michaelc9802@gmail.com>
2020-01-17 19:49:57 -0800

    win alg

--- Components/Game/game.js
+++ Components/Game/game.js
@@ -25,13 +25,33 @@ export default class Game extends React.Component {
         });
       }
 
+      win(b) {
+        const l = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6]
+        ];
+        for (let i = 0; i < l.length; i++) {
+          const [x, y, z] = l[i];
+          if (b[x] && b[x] === b[y] && b[x] === b[z]) {
+            return b[x];
+          }
+        }
+        return null;
+      }
+
       reset() {
         this.setState({
           step: 0,
           X: 1
         });
       }
-      
+
     render() {
         let status;
         if (winner) {
